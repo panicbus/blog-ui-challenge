b801b63245be1e7cdd00a714935a3470df2f333d
panicbus <elementalair@gmail.com>
2015-06-02 01:29:09 -0700

    added handlebars, display post from list working

--- ui-blog/src/main/webapp/js/application.js
+++ ui-blog/src/main/webapp/js/application.js
@@ -1,1 +1,38 @@
-application.js
+$(document).ready(function(){
+
+	$('#postList').on('click', 'a.post-list-post', function(e){
+		e.preventDefault();
+		var $this = $(this);
+		var postId = $(this).attr('data-id');
+		console.log(postId);
+		$('.blog-content').empty();
+
+		var get_request = $.ajax({
+			url: '../Blog/api',
+      type: 'get',
+      dataType: 'json',
+      data: postId
+
+    });
+
+    get_request.done(function(data){
+
+			$(data.blog.posts).each(function(index, element){
+		    if(element.id == postId){
+			    console.log(element.title);
+			    console.log(element.text);
+		    	blogTitle = element.title;
+		    	blogText = element.text;
+			  }
+			})
+
+      $('.blog-content').append('<div class="content-area-title">' +
+      														blogTitle +
+      													'</div><div class="content-area-text">' +
+      														blogText +
+      													'</div>'
+      													)
+    });
+	});
+
+})
